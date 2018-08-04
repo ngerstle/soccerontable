ca4550fe26f6fc47d872fcb72b512fdb6a57309f
Nicholas Gerstle <nicholas.gerstle@cognite.com>
2018-08-04 12:19:10 +0200

    moving soccerontable setup into dockerfile

--- docker/runsoccercode.sh
+++ docker/runsoccercode.sh
@@ -1,61 +1,15 @@
 #!/usr/bin/env bash
 
 
-# expected environment variables...
-export BASEDIR="/opt"
-export SOCCERCODE="$BASEDIR/soccerontable"
-export DATADIR="$BASEDIR/datadir"
-export DETECTRON="$BASEDIR/detectron"
-export OPENPOSEDIR="$BASEDIR/openpose"
-export MODELPATH="$BASEDIR/model.pth"
-
-export PYTHONPATH="$PYTHONPATH:$SOCCERCODE:$OPENPOSEDIR:$DETECTRON"
-##--     Interpreter:                 /usr/bin/python3 (ver 3.5.2)
-##--     packages path:               lib/python3.5/dist-packages
- 
+#export PYTHONPATH="$PYTHONPATH:$SOCCERCODE:$OPENPOSEDIR:$DETECTRON"
+###--     Interpreter:                 /usr/bin/python3 (ver 3.5.2)
+###--     packages path:               lib/python3.5/dist-packages
+# 
  
 set -e 
 
 
-
-
-## install soccercode
-cd $BASEDIR
-git clone https://github.com/ngerstle/soccerontable.git $SOCCERCODE
-
-cd ${SOCCERCODE}
-chmod u+x compile.sh
-./compile.sh
-
-cd ${SOCCERCODE}/utils/nms/
-python3 setup.py build_ext --inplace
-python2 setup.py build_ext --inplace
-
-cd ${SOCCERCODE}
-pip3 install -r requirements.txt
-pip2.7 install -r requirements.txt
-
-## get example dataset
-mkdir -p $DATADIR
-cd $BASEDIR
-wget http://grail.cs.washington.edu/projects/soccer/barcelona.zip && unzip barcelona.zip && mv barcelona/*  $DATADIR
-
-
-
-
-############## MANUAL CHANGES #########
-# comment out detectron/modeling/detector.py
-# -46;+46 : #assert
-
-
-
-pip2.7 install pycocotools h5py
-pip3 install pycocotools h5py
-
 ## run detectron
-mkdir -p  $DATADIR/detectron
-cd $SOCCERCODE
-cp utils/thirdpartyscripts/infer_subimages.py $DETECTRON/tools/
 cd $DETECTRON 
 python2 tools/infer_subimages.py --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml --output-dir $DATADIR/detectron --image-ext jpg --wts https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl $DATADIR/images/
 echo "DONE: DETECTRON"
@@ -67,23 +21,6 @@ python3 demo/calibrate_video.py --path_to_data $DATADIR
 echo "DONE: CALIBRATION"
 
 # Next, we estimate poses, near the bounding boxes that Mask-RCNN gave.
-#
-#
-#TODO add following to soccer3d/core.py in def estimate_poses()
-#            def opencv_matrix(loader, node):
-#                mapping = loader.construct_mapping(node)
-#                mat = np.array(mapping["data"])
-#                mat.resize(mapping["rows"], mapping["cols"])
-#                return mat
-#
-#            yaml.add_constructor(u"tag:yaml.org,2002:opencv-matrix", opencv_matrix)
-#
-#TODO add if check in soccer3d/core.py def estimate_poses():
-#	    posefilepath = join(join(self.path_to_dataset, 'tmp'), '{0}_pose.yml'.format(j))
-#                   if os.path.isfile(posefilepath):
-#                        with open(posefilepath) as data_file:
-#
-
 
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/
