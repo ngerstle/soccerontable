eebd46a84071666ba0b3c8d7892ada1fe156cdca
Nicholas Gerstle <nicholas.gerstle@cognite.com>
2018-08-04 12:50:57 +0200

    width cleanup

--- docker/runsoccercode.sh
+++ docker/runsoccercode.sh
@@ -11,27 +11,35 @@ set -e
 
 ## run detectron
 cd $DETECTRON 
-python2 tools/infer_subimages.py --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml --output-dir $DATADIR/detectron --image-ext jpg --wts https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl $DATADIR/images/
+python2 tools/infer_subimages.py \
+	--cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
+	--output-dir $DATADIR/detectron \
+	--image-ext jpg \
+	--wts https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl \
+	$DATADIR/images/
+
 echo "DONE: DETECTRON"
 
 
-# Now we can run the calibration step. In the first frame we give 4 manual correspondences and afterwards the camera parameters are optimized to fit a synthetic 3D field to the lines in the image.
+# run the calibration step
 cd $SOCCERCODE
 python3 demo/calibrate_video.py --path_to_data $DATADIR
 echo "DONE: CALIBRATION"
 
 
-# Next, we estimate poses, near the bounding boxes that Mask-RCNN gave.
+# estimate poses
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/
 cd $SOCCERCODE
 python3 demo/estimate_poses.py --path_to_data $DATADIR --openpose_dir $OPENPOSEDIR
 echo "DONE: ESTIMATE POSES"
 
 
-# The estimated poses cover very well the players in terms of localization/extend/etc. We use them to make individual crops of players for every frame for further processing. We use also the poses to refine the instance segmentation.
+# make individual crops of players for every frame 
 cd ${SOCCERCODE}
 python3 demo/crop_players.py --path_to_data $DATADIR
 echo "DONE: CROP PLAYERS"
+
+# use the poses to refine instance segmentation.
 export OMP_NUM_THREADS=8
 ./soccer3d/instancesegm/instancesegm --path_to_data $DATADIR/players/ --thresh 1.5 --path_to_model ./soccer3d/instancesegm/model.yml.gz
 echo "DONE: instancesegm 2"
@@ -47,13 +55,13 @@ python3 soccer3d/soccerdepth/test.py --path_to_data $DATADIR/players --modelpath
 echo "DONE: test depth"
 
 
-# Next, we convert the estimated depthmaps to pointclouds.
+#  convert the estimated depthmaps to pointclouds.
 cd ${SOCCERCODE}
 python3 demo/depth_estimation_to_pointcloud.py --path_to_data $DATADIR
 echo "DONE: POINTCLOUD"
 
 
-# Finally we generate one mesh per frame, with the smooth position of the players, based on tracking. Note that the resolution of the mesh is reduced, so later it can easily fit into Hololens.
+# generate one mesh per frame, with the smooth position of the players, based on tracking
 cd ${SOCCERCODE}
 python3 demo/track_players.py --path_to_data $DATADIR
 echo "DONE: TRACK PLAYERS"
